3126eaacd085d0b039290ad85ffb6a83b0ff1e64
alextaylor04 <at1587kc@gmail.com>
2025-11-07 23:07:09 -0600

    Added player with movement & progress bar for kicks.

---
+++ scenes/Animated_Sprites/character_body_2d.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -300.0
+
+
+func _physics_process(delta: float) -> void:
+	var animated_sprite = $AnimatedSprite2D
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#animated_sprite.play("Jump") 
+		velocity.y = JUMP_VELOCITY
+		
+		
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+		animated_sprite.play("Move")
+		
+		if direction < 0:
+			animated_sprite.flip_h = true
+		else:
+			animated_sprite.flip_h = false
+			
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if velocity.x == 0 and velocity.y == 0:
+			animated_sprite.play("Idle")
+
+	move_and_slide()
---
+++ scenes/kick_charge.gd
@@ -0,0 +1,27 @@
+extends ProgressBar
+
+
+var charging = false
+var dt_charge = 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("kick_charge"):
+		charging = true
+	elif Input.is_action_just_released("kick_charge"):
+		#handle kick
+		charging = false
+		#now we have dt_charge to represent how strong the kick was
+		print(dt_charge)
+		dt_charge = 0 #reset charge
+	if charging:
+		dt_charge += delta
+		if dt_charge > 1.0:
+			dt_charge = 1.0
+		self.value = dt_charge * 100
+		
