7a3fed1e4ada3cab3e7a9b431cd1a95ad8058f61
Alex Crossen <misalignedstack@gmail.com>
2025-11-08 14:37:40 -0600

    modify kick charge component

--- scripts/exit_button.gd
+++ scripts/exit_button.gd
@@ -4,7 +4,6 @@ extends Button
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
