aa43a947264d6e6903b2a2d9ced06ae9314f14f3
Alex Crossen <misalignedstack@gmail.com>
2025-11-08 21:44:16 -0600

    kick anim

--- scenes/Animated_Sprites/character_body_2d.gd
+++ scenes/Animated_Sprites/character_body_2d.gd
@@ -5,38 +5,51 @@ class_name Player
 const SPEED = 150.0
 const JUMP_VELOCITY = -600.0
 const JUMP_SOUND = preload("res://sounds/jump.mp3")
+@onready var animated_sprite = $AnimatedSprite2D
+
+var kicking = false
 
 func _ready() -> void:
 	var camera = $Camera2D
 	camera.make_current()
 
 func _physics_process(delta: float) -> void:
-	var animated_sprite = $AnimatedSprite2D
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#animated_sprite.play("Jump") 
 		velocity.y = JUMP_VELOCITY
 		G.SoundHelpers.play_async(self, JUMP_SOUND)
+
+	var kicking = animated_sprite.animation == "Kick"
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-		animated_sprite.play("Move")
+		
+		if !kicking:
+			animated_sprite.play("Move")
 		
 		if direction < 0:
 			animated_sprite.flip_h = true
 		else:
 			animated_sprite.flip_h = false
-			
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if velocity.x == 0 and velocity.y == 0:
-			animated_sprite.play("Idle")
+	
+	if velocity.x == 0 and velocity.y == 0 and !kicking:
+		animated_sprite.play("Idle")
+	#if velocity.x == 0 and velocity.y == 0 and !kicking:
+	#	animated_sprite.play("Idle")
 
 	move_and_slide()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	animated_sprite.play("Idle")
+	pass # Replace with function body.
