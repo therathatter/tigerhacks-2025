0b26e4ac5c998a01069d20084a7b1b318d41375e
alextaylor04 <at1587kc@gmail.com>
2025-11-08 22:42:31 -0600

    Working on reset functionality

--- scripts/maps_controller.gd
+++ scripts/maps_controller.gd
@@ -10,13 +10,30 @@ var kick_charge: KickCharge
 var player_node: CharacterBody2D
 var tilemaps_container: TilemapsContainer
 
+var initial_player_pos: Vector2
+var initial_maps_pos = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player_node = $"../Player".get_character_body()
+	initial_player_pos = player_node.position
 	kick_charge = $"../Player".get_kick_charge()
 				
 	if tilemaps_path:
 		tilemaps_container = get_node(tilemaps_path)
+		for tilebody in tilemaps_container.get_children():
+			if tilebody is not RigidBody2D:
+				continue
+			initial_maps_pos.append(tilebody.position)
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("reset_level"):
+		player_node.position = initial_player_pos
+		var i = 0
+		for tilebody in tilemaps_container.get_children():
+			if tilebody is not RigidBody2D:
+				continue
+			tilebody.position = initial_maps_pos[i]
+			i += 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
