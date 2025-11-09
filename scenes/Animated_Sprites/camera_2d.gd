2da59d46bb9608a1c7d3292c94dd6a907b01c029
Alex Crossen <misalignedstack@gmail.com>
2025-11-09 01:43:16 -0600

    scaling fixes

--- scenes/Animated_Sprites/camera_2d.gd
+++ scenes/Animated_Sprites/camera_2d.gd
@@ -9,14 +9,13 @@ func get_h():
 	return DisplayServer.window_get_size().y
 
 func z():
-	return sqrt((get_w() ** 2) + (get_h() ** 2)) / 768.0
+	if get_tree().current_scene.name == "TutorialLevel":
+		return sqrt((get_w() ** 2) + (get_h() ** 2)) / 768.0
+	else:
+		return sqrt((get_w() ** 2) + (get_h() ** 2)) / 1296.0
 
 # Called when the noade enters the scene tree for the first time.
 func _ready() -> void:
-	# hacky, but WORKS!!!!
-	# base it on the base vport
-	# lastX = 1152
-	_process(0.0)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
