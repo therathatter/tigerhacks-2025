1a847f1261d1d0ccdf8dd5e9f26a9bfd258829c8
Alex Crossen <misalignedstack@gmail.com>
2025-11-08 19:09:39 -0600

    add some sounds

--- scripts/kick_charge.gd
+++ scripts/kick_charge.gd
@@ -5,15 +5,29 @@ var charging = false
 var allowed_to_charge = true
 var dt_charge = 0.0
 
+var kick_sound = preload("res://sounds/kick.wav")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
-
+	
+func play_sound_async(sound):
+	var music = AudioStreamPlayer.new()
+	music.set_stream(sound)
+	music.volume_db = 1
+	music.pitch_scale = 1
+	self.add_child(music)
+	music.play()
+	await music.finished
+	self.remove_child(music)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("kick_charge") and allowed_to_charge:
+	if Input.is_action_just_pressed("kick_charge") and allowed_to_charge and !$KickAudioStream.playing:
 		charging = true
 	elif Input.is_action_just_released("kick_charge"):
+		if charging:
+			play_sound_async(kick_sound)
 		#handle kick
 		charging = false
 		allowed_to_charge = false
--- scripts/play_button.gd
+++ scripts/play_button.gd
@@ -1,14 +1,20 @@
 extends Button
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+var checkedController = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	# Godot bug hack
+	if !checkedController and Input.get_connected_joypads().size() != 0:
+		if Input.get_connected_joypads().size() != 0:
+			self.grab_focus.call_deferred()
+		checkedController = true
 	pass
+	
 
 func _pressed() -> void:
 	$"../../AnimationPlayer".play("fade")
